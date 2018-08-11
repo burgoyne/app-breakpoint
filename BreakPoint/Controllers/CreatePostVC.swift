0eeafa330bc88212ddadecccb21e862e931b8a66
Andre Burgoyne <andreburgoyne@Andres-MacBook-Pro.local>
2018-08-11 13:38:46 -0400

    feed cells now pull username

--- BreakPoint/Controllers/CreatePostVC.swift
+++ BreakPoint/Controllers/CreatePostVC.swift
@@ -23,6 +23,11 @@ class CreatePostVC: UIViewController {
         sendBtn.bindToKeyboard()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.emailLbl.text = Auth.auth().currentUser?.email
+    }
+    
     @IBAction func sendBtnWasPressed(_ sender: Any) {
         if textView.text != nil && textView.text != "//say something here" {
             sendBtn.isEnabled = false
