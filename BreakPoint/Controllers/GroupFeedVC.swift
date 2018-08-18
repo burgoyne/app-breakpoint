de576f477ae89c9f479fb56de5d8804baff3031a
Andre Burgoyne <andreburgoyne@Andres-MacBook-Pro.local>
2018-08-17 21:29:02 -0400

    build group feed - able to pull user info for feed

---
+++ BreakPoint/Controllers/GroupFeedVC.swift
@@ -0,0 +1,45 @@
+//
+//  GroupFeedVC.swift
+//  BreakPoint
+//
+//  Created by Andre Burgoyne on 2018-08-17.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var membersLbl: UILabel!
+    @IBOutlet weak var sendBtnView: UIView!
+    @IBOutlet weak var messageTextField: InsetTextField!
+    
+    var group: Group?
+    
+    func initData(forGroup group: Group) {
+        self.group = group
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sendBtnView.bindToKeyboard()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        groupTitleLbl.text = group?.groupTitle
+        DataService.instance.getEmailsFor(group: group!) { (returnedEmails) in
+            self.membersLbl.text = returnedEmails.joined(separator: ", ")
+        }
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ BreakPoint/Views/GroupFeedCell.swift
@@ -0,0 +1,22 @@
+//
+//  GroupFeedCell.swift
+//  BreakPoint
+//
+//  Created by Andre Burgoyne on 2018-08-17.
+//  Copyright © 2018 Andre Burgoyne. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+}
